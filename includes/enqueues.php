6c51bbf3531f40a529a5f5955db622081451996b
Joey <jpg@neilpie.co.uk>
2023-07-06 13:52:24 +0100

    settings section functions complete, error handling etc

--- includes/enqueues.php
+++ includes/enqueues.php
@@ -29,8 +29,17 @@ function enqueues_frontend() {
             $filename = substr( $file, strrpos( $file, '/' ) + 1 );
             wp_enqueue_script( $filename, $enqueue_path . 'js/' . $filename, array(), false, true );
             wp_localize_script( $filename, 'my_account_app', array(
-                'user_id' => get_current_user_id(),
-                'domains' => get_user_meta( get_current_user_id(), '_pie_testing_domains', true ) ? get_user_meta( get_current_user_id(), '_pie_testing_domains', true ) : array(),
+                'domains'                         => get_user_meta( get_current_user_id(), '_domains', true ) ? get_user_meta( get_current_user_id(), '_domains', true ) : array(),
+                'selected_domain'                 => get_user_meta( get_current_user_id(), '_selected_domain', true ) ? get_user_meta( get_current_user_id(), '_selected_domain', true ) : '',
+                'domain_already_exists_message'   => __( 'Domain has already been added.  Please select it from the dropdown and click save to activate it.', 'pie-testing-platform' ),
+                'domain_invalid_message'          => __( 'Domain is not a valid URL.', 'pie-testing-platform' ),
+                'domain_added_message'            => __( 'Domain has been successfully added to your list.', 'pie-testing-platform' ),
+                'domain_not_selected_message'     => __( 'No domain selected.', 'pie-testing-platform' ),
+                'domain_removed_message'          => __( 'Selected domain has been successfully removed from your list.', 'pie-testing-platform' ),
+                'settings_saved_message'          => __( 'Your settings have been updated', 'pie-testing-platform' ),
+                'settings_error_message_intro'    => __( 'Your settings were not updated and the following message was returned: ', 'pie-testing-platform' ),
+                'settings_generic_error_outro'    => sprintf( __( 'If the problem persists please %scontact us%s.', 'pie-testing-platform' ), '<a href="#">', '</a>' ),
+                'settings_error_no_message_intro' => __( 'An unknown error occured and your settings were not updated.', 'pie-testing-platform' ),
             ));
         }
 
--- src/apps/my-account-app/components/Settings.js
+++ src/apps/my-account-app/components/Settings.js
@@ -1,38 +1,72 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import validator from 'validator'
 import Spinner from './Spinner';
 
 function Settings() {
-    // Handlers for the domain in th input field
-    const [domain, updateDomain]              = useState('');
+    // Handlers for the domain in the input field
+    const [domain, updateDomain]              = useState( '' );
     // Handlers for the selected domain in the select field
-    const [selectedDomain, setSelectedDomain] = useState('');
+    const [selectedDomain, setSelectedDomain] = useState( window.my_account_app.selected_domain );
     // Domains loaded and updated as domain options for the select field
     const [domains, updateDomains]            = useState( window.my_account_app.domains );
     // Are we saving the settings?
-    const [isSaving, setSavingStatus]     = useState( false );
+    const [isSaving, setSavingStatus]         = useState( false );
+    // Notification error/message
+    const [message, updateMessage]            = useState( '' );
     
+    /**
+     * Takes the URL from the input, checks if it already exists in the select options and adds it if it doesn't
+     * 
+     * @param {object} e event
+     */
     function addDomain( e ) {
-        // @todo validate domain is URL and check it doesn't already exist
         e.preventDefault();
-        updateDomains( domains => [...domains, domain] );
+        updateMessage( '' );
+        if( ! isValidUrl( domain ) ) {
+            updateMessage( window.my_account_app.domain_invalid_message );
+        } else if ( domains.indexOf( domain ) > -1 ) {
+            updateMessage( window.my_account_app.domain_already_exists_message );
+        } else {
+            updateDomains( domains => [...domains, domain] );
+            updateDomain( '' );
+            updateMessage( window.my_account_app.domain_added_message );
+        }
     };
 
+    /**
+     * Removes the currently selected URL from the domains dropdown
+     * 
+     * @param {object} e event 
+     */
     function removeDomain( e ) {
         e.preventDefault();
-        updateDomains( domains => {
-            const index = domains.indexOf( selectedDomain );
-            if ( index !== -1 ) {
-                domains.splice( index, 1 );
-            }
-            return domains;
-        } );
-        setSelectedDomain('');
+        updateMessage( '' );
+        if ( '' == selectedDomain ) {
+            // No domain selected
+            updateMessage( window.my_account_app.domain_not_selected_message );
+        } else {
+            updateDomains( domains => {
+                const index = domains.indexOf( selectedDomain );
+                if ( index !== -1 ) {
+                    domains.splice( index, 1 );
+                }
+                return domains;
+            } );
+            updateMessage( window.my_account_app.domain_removed_message );
+            setSelectedDomain('');
+        }
     };
 
-    // Send 
+    /**
+     * Send request to update usermeta with the provided domains
+     * (saves the domains list as well as the currently selected domain)
+     * 
+     * @param {object} e event 
+     */
     const saveSettings = async ( e ) => {
         e.preventDefault();
+        updateMessage( '' );
         setSavingStatus( true );
 
         // Set up the Axios instance with interceptors
@@ -47,6 +81,7 @@ function Settings() {
         });
     
         try {
+            // me = current user
             const response = await axiosInstance.post(
                 window.wpApiSettings.root + 'wp/v2/users/me', {
                     'meta' : {
@@ -56,25 +91,46 @@ function Settings() {
                 }
             );
         
-            if ( ! response.ok ) {
-                setSavingStatus( false );
+            // Response not 200
+            if ( 200 != response.status ) {
+                console.log( response );
                 throw new Error( 'Failed to update user testing settings.' );
             }
 
-            console.log( 'User testing settings updated successfully.' );
+            // Updated metadata successfully
             setSavingStatus( false );
+            updateMessage( window.my_account_app.settings_saved_message );
 
         } catch ( error ) {
-            console.error( error.message );
+            // Error
             setSavingStatus( false );
+            updateMessage( window.my_account_app.settings_error_message_intro + error.message + '.  ' + window.my_account_app.settings_generic_error_outro );
         }
     };
+
+    /**
+     * Checks given URL is valid format
+     * 
+     * @param {string} url 
+     * @returns boolean
+     */
+    const isValidUrl = ( url ) => {
+        if ( validator.isURL( url ) ) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
     return (
         <div className='content-wrap'>
+            { !! message && <div className='message-wrap'>
+                <p>{message}</p><span onClick={e => updateMessage('')}>X</span>
+            </div> }
             <form onSubmit={saveSettings}>
                 <fieldset>
                     <label>
-                        <input type="text" name="new-domain" onChange={e => updateDomain(e.target.value)} value={domain} />
+                        <input type="url" name="new-domain" onChange={e => updateDomain(e.target.value)} value={domain} />
                         Add New Domain
                     </label>
                     <button id="add-domain" onClick={e => addDomain( e )} disabled={isSaving}>Add</button>
@@ -82,7 +138,7 @@ function Settings() {
                 <fieldset>
                     <label>Select domain to test:
                         <select name="domains" value={selectedDomain} onChange={e => setSelectedDomain(e.target.value)}>
-                            <option value="null">Select a Domain...</option>
+                            <option value="">Select a Domain...</option>
                             {domains.map( currentDomain => (
                                 <option key={currentDomain} value={currentDomain}>
                                     {currentDomain}
