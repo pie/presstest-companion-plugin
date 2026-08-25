ad121c4a9b06d90c09abc29fe827ed76913f0928
Anna Werno <anna@pie.co.de>
2026-08-25 15:35:58 +0100

    specify checkout

--- src/apps/presstest-app/data/tests.js
+++ src/apps/presstest-app/data/tests.js
@@ -9,7 +9,7 @@ export const TEST_SUITES = [
 	{ name: 'WooCommerce Core',  value: 'woocommerce-core' },
 	{ name: 'Ascott Analytical', value: 'ascott-analytical' },
 	{ name: 'PDS Printing',      value: 'pds-printing' },
-	{ name: 'V&Me',      		 value: 'vandme' },
+	{ name: 'V&Me Checkout',      		 value: 'vandme' },
 ];
 
 /**
